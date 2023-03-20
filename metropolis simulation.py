f3f0bbe346fb933fa699285d4f155f214c4aece9
harryelliiott <2033551@swansea.ac.uk>
2023-03-20 13:49:00 +0000

    Update metropolis simulation.py

--- metropolis simulation.py
+++ metropolis simulation.py
@@ -11,12 +11,15 @@ def H(x): # defining the Hamiltonian
 configurations = 100000
 x = 0
 d = 0.1
+b = 1
 
-def met_alg(x_initial, d): 
+def met_alg(x_initial, d, b): 
     # d = amount that x is changed by 
     x_new = x + np.random.uniform(-d,d) # interval of fixed width, 2d 
     H_change = H(x_new) - H(x)
     if H_change < 0:
+        return x_new
+    elif np.exp(-(b)*H_change) > np.random.uniform(0,1):
         return x_new
     elif H_change > 0:
         return x 
@@ -24,7 +27,13 @@ def met_alg(x_initial, d):
 x_array = np.zeros(int(configurations)) # storage to be filled with values of x 
 
 for n in range(configurations):
-    x = met_alg(x, d)
+    x = met_alg(x, d, b)
     x_array[n] = x
     
+
 print(x_array)
+xax = np.zeros(int(configurations))
+for n in range(configurations):
+    xax[n] = n+1
+    import matplotlib.pyplot as plt
+plt.plot(xax, x_array)
