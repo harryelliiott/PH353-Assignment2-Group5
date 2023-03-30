46fd70346ba4c8d5e04f439da0924629173fb2f6
2000683 <2000683@swansea.ac.uk>
2023-03-30 17:39:37 +0100

    Problem solved
    Managed to figure out the problems were were having, some new ones arose but were dealt with, we now have a graph that produces data, not sure if its right yet, but now that the code functionally works we can start to fine tune it

--- Most updated.py
+++ Most updated.py
@@ -1,7 +1,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 def met_alg(x, d, beta): 
     # d = amount that x is changed by 
     x_new = x + np.random.uniform(-d,d) # interval of fixed width, 2d 
@@ -20,17 +19,18 @@ def H(x): # defining the Hamiltonian
 def U(x,d,beta, t, configurations):
     while configurations % t != 0:
         configurations = configurations + 1
+    x_array = np.zeros(int(configurations))  # storage to be filled with values of x 
     for n in range(configurations):
-        x = met_alg(x, d, beta)
-        x_array[n] = x
+        
+        x_array[n] = met_alg(x, d, beta)
     z_array = np.zeros(int(configurations/t))
     U_array = np.zeros(int(configurations/t))
     placeholder1 = np.zeros(int(t))
     placeholder2 = np.zeros(int(t))
     for n in range(int(configurations/t)):
         for i in range(t):
-            placeholder1[i] = np.exp(-beta*H(x_array[t*i+1]))
-            placeholder2[i] = H(x_array[t*i+1]) * np.exp(-beta*H(x_array[t*i+1]))
+            placeholder1[i] = np.exp(-beta*H(x_array[t*n+i]))
+            placeholder2[i] = H(x_array[t*n+i]) * np.exp(-beta*H(x_array[t*n+i]))
         z_array[n] = sum(placeholder1)/len(placeholder1)
         U_array[n] = sum(placeholder2)/len(placeholder2)
     Z = sum(z_array)/len(z_array)
@@ -49,17 +49,13 @@ beta = 5
 
         
     
-x_array = np.zeros(int(configurations)) # storage to be filled with values of x 
-
-for n in range(configurations):
-    x = met_alg(x, d, beta)
-    x_array[n] = x
-
-
+U_data = np.zeros((max_tau-1,2))
+for t in range (1,max_tau):
+    ext_U_array = U(x,d,beta,t,configurations)
+    U_data[t-1,0] = ext_U_array[0,0]
+    U_data[t-1,1] = ext_U_array[0,1]
 
 
-U = [U(x,d,beta,t,configurations)[0,0] for t in range(1,max_tau)]
-delta_U = [U(x,d,beta,t,configurations)[0,1] for t in range(1,max_tau)]     
-plt.errorbar(range(len(U)),U, delta_U)
+    
+plt.errorbar(range(1,max_tau),U_data[:,0], U_data[:,1])
         
-print(x_array)
