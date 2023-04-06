29e146033001fdae21a76ea53a96961d125dcf81
2000683 <2000683@swansea.ac.uk>
2023-04-06 15:17:58 +0100

    Slight adjustment to histogram

--- Histogram.py
+++ Histogram.py
@@ -8,9 +8,9 @@ def H(x): # defining the Hamiltonian
     return H
 
 configurations = 100000
-x = 0
-d = 0.1
-b = 1
+x = 1
+d = 1
+b = 50
 
 def met_alg(x, d, beta): 
     # d = amount that x is changed by 
@@ -36,9 +36,8 @@ xax = np.zeros(int(configurations))
 for n in range(configurations):
     xax[n] = n+1
 import matplotlib.pyplot as plt
-plt.hist(x_array, bins = 50, density = True)
+plt.hist(x_array, bins = 20, density = True)
 plt.xlabel('H(x)')
-plt.ylabel('probability')
+plt.ylabel('Probability density')
 plt.title("Gaussian distribution")
 plt.savefig('Gaussian_Distribution.pdf')
-
